ca51b60bc2f6ad176eedf662a71010b0146a16b1
wolfson-1 <harrywolverson@googlemail.com>
2022-11-24 23:25:30 +0000

    FUNCTION TO ADD USER INPUTS TO NEW HTML DIV ADDED: Add new HTML div created using new book from user input as well as push to array of myLibrary.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 /* query selectors */
-const libraryContainer = document.querySelector('.libraryContainer')
+const libraryContainer = document.querySelector('.libraryContainer');
+const submitButton = document.querySelector('.submitButton');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
 
+/* variables */
+let newTitle = '';
+let newAuthor = '';
+let newPages = '';
+let newRead = '';
+let newBook = '';
 
 //------------------------------------------
 
@@ -32,15 +43,64 @@ let myLibrary = [
 ]
 
 function bookConstructor(title,author,pages,read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
     this.read = read
+    this.pages = pages
+    this.author = author
+    this.title = title
 };
 
 //take inputs from user & create new book object
 
+function libraryDivUpdate() { {
 
+        let libraryBook = document.createElement('div');
+        libraryBook.classList.add(`libraryBook${myLibrary.length - 1}`);
+        libraryContainer.appendChild(libraryBook);
+
+        let bookTitle = document.createElement('div');
+        bookTitle.classList.add('title');
+        libraryBook.appendChild(bookTitle);
+        bookTitle.innerHTML = `Title: ${myLibrary[myLibrary.length - 1].title}`
+
+        let bookAuthor = document.createElement('div');
+        bookAuthor.classList.add('author');
+        libraryBook.appendChild(bookAuthor);
+        bookAuthor.innerHTML = `Author: ${myLibrary[myLibrary.length - 1].author}`
+
+        let bookPages = document.createElement('div');
+        bookPages.classList.add('pages');
+        libraryBook.appendChild(bookPages);
+        bookPages.innerHTML = `No. of Pages: ${myLibrary[myLibrary.length - 1].pages}`
+
+        let bookRead = document.createElement('div');
+        bookRead.classList.add('read');
+        libraryBook.appendChild(bookRead);
+        bookRead.innerHTML = `Read?: ${myLibrary[myLibrary.length - 1].read}`
+}};
+
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //assigns form value to temporary variables
+    newTitle = titleInput.value;
+    newAuthor = authorInput.value;
+    newPages = pagesInput.value;
+    newRead = readInput.value;
+
+    //adds temporary variables to newBook & pushes to myLibrary array
+    newBook = new bookConstructor(newTitle,newAuthor,newPages,newRead);
+    addToLibrary(newBook);
+
+    //Clears all temporary variables
+    newTitle = '';
+    newAuthor = '';
+    newPages = '';
+    newRead = '';
+    newBook = '';
+
+    libraryDivUpdate();
+})
 
 
 
@@ -63,7 +123,6 @@ display in library
 */
 
 //creates divs for each book & adds library array info
-
 for (i=0; i < myLibrary.length; i++) {
     let libraryBook = document.createElement('div');
     libraryBook.classList.add(`libraryBook${i}`);
@@ -90,3 +149,4 @@ for (i=0; i < myLibrary.length; i++) {
     bookRead.innerHTML = `Read?: ${myLibrary[i].read}`
     
 };
+
